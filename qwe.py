8e4052051dce56edb3a83485ed275fa1981190b7
PectiN <rperdol@mail.ru>
2022-08-18 16:53:33 +0300

    added tests generation

--- qwe.py
+++ qwe.py
@@ -62,6 +62,7 @@ output_nodes = 10
 
 neural_number = 5
 epochs_number = 3
+tests_number = 9
 
 l_r = [0.9, 0.7, 0.5, 0.3, 0.1]
 
@@ -76,36 +77,38 @@ test_data_file = open("mnist_dataset\mnist_test_10.csv", 'r')
 test_data_list = test_data_file.readlines()
 test_data_file.close()
 
-for e in range(epochs_number):
-    i = 0
-    procent = 1
-    print("Epoch number:", e)
-    for record in data_list:
-        if i == 0: print("start training")
-        elif i == int(len(data_list) * (procent / 100)):
-            if procent % 10 == 0: print(procent, "%", end = '', sep = '')
-            else: print(".", end = '', sep = '')
-            procent += 1
-        elif i == len(data_list) - 1: print("\ntraining done\n")
-        i += 1
+for test in range(tests_number):
+    print("Test number:", test)
+    for e in range(epochs_number):
+        i = 0
+        procent = 1
+        print("Epoch number:", e)
+        for record in data_list:
+            if i == int(len(data_list) * (procent / 100)):
+                if procent % 10 == 0: print(procent, "%", end = '', sep = '')
+                else: print(".", end = '', sep = '')
+                procent += 1
+            # elif i == 0: print("start training")
+            # elif i == len(data_list) - 1: print("\ntraining done\n")
+            i += 1
 
-        all_values = record.split(',')
-        inputs = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
-        targets = numpy.zeros(output_nodes) + 0.01
-        targets[int(all_values[0])] = 0.99
-        for j in range(neural_number):
-            n[j].train(inputs, targets)
+            all_values = record.split(',')
+            inputs = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
+            targets = numpy.zeros(output_nodes) + 0.01
+            targets[int(all_values[0])] = 0.99
+            for j in range(neural_number):
+                n[j].train(inputs, targets)
 
-    for i in range(neural_number):
-        answ = [0, 0]
-        wrong = 0
-        right = 0
-        for testing in test_data_list:
-            all_values = testing.split(',')
-            answ = n[i].query((numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01, all_values[0])
-            wrong += answ[0]
-            right += answ[1]
-        print("For learning rate", l_r[i])
-        # print("Number of wrong ansvers", wrong, "/ 10")
-        # print("Number of right ansvers", right, "/ 10")
-        print("Effectiveness: ", right / output_nodes * 100, "%")
+        for i in range(neural_number):
+            answ = [0, 0]
+            wrong = 0
+            right = 0
+            for testing in test_data_list:
+                all_values = testing.split(',')
+                answ = n[i].query((numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01, all_values[0])
+                wrong += answ[0]
+                right += answ[1]
+            print("For learning rate", l_r[i])
+            # print("Number of wrong ansvers", wrong, "/ 10")
+            # print("Number of right ansvers", right, "/ 10")
+            print("Effectiveness: ", right / output_nodes * 100, "%")
